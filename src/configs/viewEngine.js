22ac1ebf5510b637f797bfded85e57bac2593e06
thmthu <thmt.thu@gmail.com>
2024-11-19 22:57:10 +0700

    update multiple route use static

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const configViewEngine = (app) => {
-    app.set('views', path.join(__dirname, '..', 'views'));
-    app.set('view engine', 'ejs');
-    app.use('/css', express.static(path.join(__dirname, '../views/css')));
-    app.use('/img', express.static(path.join(__dirname, '../views/img')));
-    app.use('/js', express.static(path.join(__dirname, '../views/js')));
-    app.use('/lib', express.static(path.join(__dirname, '../views/lib')));
-    app.use('/mail', express.static(path.join(__dirname, '../views/mail')));
-    app.use('/scss', express.static(path.join(__dirname, '../views/scss')));
-    app.use('/partials', express.static(path.join(__dirname, '../views/partials')));
+  app.set("views", path.join(__dirname, "..", "views"));
+  app.set("view engine", "ejs");
+
+  const staticPaths = ["css", "img", "js", "lib", "mail", "scss", "partials","assets"];
+
+  staticPaths.forEach((staticPath) => {
+    app.use(
+      `/${staticPath}`,
+      express.static(path.join(__dirname, `../views/${staticPath}`))
+    );
+    app.use(
+      `/products-list-by-key/${staticPath}`,
+      express.static(path.join(__dirname, `../views/${staticPath}`))
+    );
+  });
 };
 
 module.exports = configViewEngine;
