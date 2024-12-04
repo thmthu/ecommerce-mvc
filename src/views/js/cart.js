1e0d5df33d4aeada1e7ca6c9e39117f9dd8ac6d9
nnt107a <22120325@student.hcmus.edu.vn>
2024-12-04 16:45:18 +0700

    update remove item

--- src/views/js/cart.js
+++ src/views/js/cart.js
@@ -1,12 +1,5 @@
+
 $(document).ready(function() {
-  function debounce(func, wait) {
-    let timeout;
-    return function(...args) {
-      const context = this;
-      clearTimeout(timeout);
-      timeout = setTimeout(() => func.apply(context, args), wait);
-    };
-  }
 
   function updateTotal() {
     let total = 0;
@@ -18,64 +11,7 @@ $(document).ready(function() {
 
   updateTotal();
 
-  $('.btn-minus').onclick(debounce(function() {
-    const productId = $(this).data('product-id');
-    const input = $(this).closest('.quantity').find('input');
-    let quantity = parseInt(input.val());
-
-    if (quantity > 1) {
-      quantity--;
-      input.val(quantity);
-
-      fetch('/cart-update', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({product: { productId: productId, quantity: quantity }})
-      })
-      .then(response => response.json())
-      .then(data => {
-        const row = input.closest('tr');
-        const price = parseFloat(row.find('td:nth-child(2)').text().replace('$', ''));
-        row.find('.total').text('$' + (price * quantity).toFixed(2));
-        updateTotal();
-      })
-      .catch(error => {
-        alert('Error updating cart.');
-      });
-    }
-  }, 300)); // 300ms delay
-
-  $('.btn-plus').onclick(debounce(function() {
-    const productId = $(this).data('product-id');
-    const input = $(this).closest('.quantity').find('input');
-    let quantity = parseInt(input.val());
-
-    quantity++;
-    input.val(quantity);
-
-    fetch('/cart-update', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({product: { productId: productId, quantity: quantity }})
-    })
-    .then(response => response.json())
-    .then(data => {
-      const row = input.closest('tr');
-      const price = parseFloat(row.find('td:nth-child(2)').text().replace('$', ''));
-      row.find('.total').text('$' + (price * quantity).toFixed(2));
-      console.log(price);
-      updateTotal();
-    })
-    .catch(error => {
-      alert('Error updating cart.');
-    });
-  }, 300)); // 300ms delay
-
-  $('.remove-from-cart').onclick(function() {
+  $('.remove-from-cart').click(function() {
     const productId = $(this).data('product-id');
     const row = $(this).closest('tr');
 
