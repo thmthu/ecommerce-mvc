fb73318693169d6cf4ddf2a20986590cb71217e5
nnt107a <22120325@student.hcmus.edu.vn>
2024-12-24 14:58:39 +0700

    update pagination

--- src/views/js/loadPage.js
+++ src/views/js/loadPage.js
@@ -64,13 +64,69 @@ function loadPage(page, sortBy = '') {
 
         // Update the pagination
         $('#pagination').html('');
-        for (let i = 1; i <= data.totalPages; i++) {
-          $('#pagination').append(`
-            <li class="page-item ${data.currentPage === i ? 'active' : ''}">
-              <a class="page-link" href="#" onclick="loadPage(${i}, '${sortBy}')">${i}</a>
-            </li>
-          `);
+        let paginationHtml = '';
+        paginationHtml += `
+          <li class="page-item ${ data.currentPage == 1 ? 'disabled' : '' }">
+            <a class="page-link" href="#" onclick="loadPage(${ data.currentPage - 1 }, '${ data.sortBy }')" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span class="sr-only">Previous</span>
+            </a>
+          </li> `
+        paginationHtml += `
+          <li class="page-item ${ data.currentPage == 1 ? 'active' : '' }">
+            <a class="page-link" href="#" onclick="loadPage(1, '${ data.sortBy }')">1</a>
+          </li> `
+        if (data.totalPages > 1) {
+          if (data.totalPages > 2) {
+            if (data.currentPage > 2 && data.currentPage < data.totalPages - 1) {
+              paginationHtml += `
+                <li class="page-item disabled">
+                  <a class="page-link" href="#">...</a>
+                </li> `
+            } else {
+              paginationHtml += `
+                <li class="page-item ${ data.currentPage == 2 ? 'active' : '' }">
+                  <a class="page-link" href="#" onclick="loadPage(2, '${ data.sortBy }')">2</a>
+                </li> `
+            }
+            if (data.totalPages > 3) {
+              if (data.totalPages > 4 && (data.currentPage <= 2 || data.currentPage >= data.totalPages - 1)) {
+                paginationHtml += `
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#">...</a>
+                  </li> `
+              } else if (data.totalPages > 4) {
+                paginationHtml += `
+                  <li class="page-item active">
+                    <a class="page-link" href="#">${ data.currentPage }</a>
+                  </li> `
+              }
+              if (data.currentPage > 2 && data.currentPage < data.totalPages - 1) {
+                paginationHtml += `
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#">...</a>
+                  </li> `
+              } else {
+                paginationHtml += `
+                  <li class="page-item ${ data.currentPage == data.totalPages - 1 ? 'active' : '' }">
+                    <a class="page-link" href="#" onclick="loadPage(${ data.totalPages - 1 }, '${ data.sortBy }')">${ data.totalPages - 1 }</a>
+                  </li> `
+              }
+            }
+          }
+          paginationHtml += `
+            <li class="page-item ${ data.currentPage == data.totalPages ? 'active' : '' }">
+              <a class="page-link" href="#" onclick="loadPage(${ data.totalPages }, '${ data.sortBy }')">${ data.totalPages }</a>
+            </li> `
         }
+        paginationHtml += `
+          <li class="page-item ${ data.currentPage == data.totalPages ? 'disabled' : '' }">
+            <a class="page-link" href="#" onclick="loadPage(${ data.currentPage + 1 }, '${ data.sortBy }')" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+              <span class="sr-only">Next</span>
+            </a>
+          </li> `
+        $('#pagination').append(paginationHtml);
       },
       error: function(err) {
         console.error('Failed to load page:', err);
