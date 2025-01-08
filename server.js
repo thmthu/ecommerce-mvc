b9b9d010d3d39141a0252bc712e54a99cdfa8a0e
Nguyễn Nhật Tân <22120325@student.hcmus.edu.vn>
2025-01-08 21:47:30 +0700

    revert 1 commit

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const app = require("./src/app");
 const config = require("./src/configs/config.mongo");
 const PORT = config.app.port;
 const fs = require("fs");
-const http2 = require('node:http2');
+const https = require('https');
 
 const httpsOptions = {
   key: fs.readFileSync("./cert/server.key"),
   cert: fs.readFileSync("./cert/server.crt"),
 };
 console.log(PORT);
-const server = http2.createServer(httpsOptions, app).listen(PORT, () => {
+const server = https.createServer(httpsOptions, app).listen(PORT, () => {
   console.log(`hello ${PORT}`);
 });
