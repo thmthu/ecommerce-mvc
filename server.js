404aa90296211e8ed027fa79e935a4d297637b51
Nguyễn Nhật Tân <22120325@student.hcmus.edu.vn>
2025-01-08 20:19:57 +0700

    modify https

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const app = require("./src/app");
 const config = require("./src/configs/config.mongo");
 const PORT = config.app.port;
 const fs = require("fs");
-const https = require("https");
+const http = require("http");
 
 const httpsOptions = {
   key: fs.readFileSync("./cert/server.key"),
   cert: fs.readFileSync("./cert/server.crt"),
 };
 console.log(PORT);
-const server = https.createServer(httpsOptions, app).listen(PORT, () => {
+const server = http.createServer(httpsOptions, app).listen(PORT, () => {
   console.log(`hello ${PORT}`);
 });
