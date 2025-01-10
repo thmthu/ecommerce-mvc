1eb649b6774bf7352f6438ea07a78cca43c85b7f
thmthu <thmt.thu@gmail.com>
2025-01-10 14:53:24 +0700

    test connect to vnpay api

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -10,6 +10,66 @@ const { ensureAuthenticated } = require("../../middleware/authMiddleware");
 const customerModel = require("../../models/customer.model");
 const { ObjectId } = require("mongodb");
 const router = express.Router();
+
+require("dotenv").config();
+const { VNPay } = require("vnpay");
+
+const vnpay = new VNPay({
+  secureSecret: process.env.VNPAY_SECURE_SECRET,
+  tmnCode: process.env.VNPAY_TMN_CODE,
+});
+router.post("", async (req, res) => {
+  // const bankList = await vnpay.getBankList();
+  // // const productTypeList = Object.entries(ProductCode).map(([key, value]) => ({ key, value }));
+  // const contentPaymentDefault = `Thanh toan don hang ${new Date().toISOString()}`;
+
+  // const { amountInput, contentPayment, productTypeSelect, bankSelect, langSelect } = req.body;
+
+  // // Validate amount
+  // if (!amountInput || amountInput <= 0) {
+  //     return res.render('home', {
+  //         scripts: `<script>alert('Số tiền chưa hợp lệ');window.location.href = '/';</script>`,
+  //         bankList,
+  //         // productTypeList,
+  //         contentPaymentDefault,
+  //     });
+  // }
+
+  // // Validate content payment
+  // if (!contentPayment) {
+  //     return res.render('home', {
+  //         scripts: `<script>alert('Vui lòng điền nội dung thanh toán');window.location.href = '/';</script>`,
+  //         bankList,
+  //         // productTypeList,
+  //         contentPaymentDefault,
+  //     });
+  // }
+
+  /**
+   * Prepare data for build payment url
+   * Note: In the VNPay documentation, it's stated that you must multiply the amount by 100.
+   * However, when using the `vnpay` package, this is done automatically.
+   */
+  const data = {
+    vnp_Amount: 100000,
+    vnp_IpAddr:
+      req.headers.forwarded ||
+      req.ip ||
+      req.socket.remoteAddress ||
+      req.connection.remoteAddress ||
+      "127.0.0.1",
+    vnp_OrderInfo: " mbnh jjg fg uy bn ",
+    vnp_ReturnUrl: "https://irrelevant-oliy-random121-e6a2fa8c.koyeb.app/login",
+    vnp_TxnRef: new Date().getTime().toString(),
+    vnp_BankCode: "",
+    vnp_Locale: "vi",
+    vnp_OrderType: "ao",
+  };
+  const url = vnpay.buildPaymentUrl(data);
+
+  return res.redirect(url);
+});
+
 passport.use(customerStrategy);
 passport.use(googleStrategy);
 
